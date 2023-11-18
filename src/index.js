cca06dc8e77a3cd1d64f05ad83c640998bfe782b
Lucille-SH <145914878+Lucille-SH@users.noreply.github.com>
2023-11-18 14:06:12 +0000

    Updates as per sandbox (search engine)
    added search engine, used starter kit from shecodes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-// Challenge 1
+function displayTemperature(response) {
+  let mainTemp = document.querySelector("#current-temperature-value");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  mainTemp.innerHTML = temperature;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "66b4t441aodafc3797bfd80f9495a36b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
 
 function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -11,25 +40,14 @@ function formatDate(date) {
     "Saturday",
   ];
 
-  let day = days[date.getDay()];
-  let hours = date.getHours();
-  let minutes = ("0" + date.getMinutes()).slice(-2);
-
-  return `${day} ${hours}:${minutes},`;
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 
-let now = new Date();
-let dayTime = document.querySelector("#day-time");
-dayTime.innerHTML = formatDate(now);
+let city = document.querySelector("#search-form");
+city.addEventListener("submit", search);
 
-// Challenge 2
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
 
-function updateCity(event) {
-  event.preventDefault();
-  let newCity = document.querySelector("#main-city");
-  let enterCity = document.querySelector("#enter-city");
-  newCity.innerHTML = enterCity.value;
-}
-
-let cityInput = document.querySelector("#city-search");
-cityInput.addEventListener("submit", updateCity);
+currentDateELement.innerHTML = formatDate(currentDate);
